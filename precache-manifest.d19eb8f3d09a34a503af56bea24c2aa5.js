d033a894420e3504aca67bd6f3263a08a8c1cc9b
22louis2 <otulouisakhigbe@gmail.com>
2020-09-12 04:07:13 +0100

    Updates

---
+++ precache-manifest.d19eb8f3d09a34a503af56bea24c2aa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63a84a24c4e28d7748d7",
+    "url": "/react-lights-out/static/css/main.285a3f54.chunk.css"
+  },
+  {
+    "revision": "63a84a24c4e28d7748d7",
+    "url": "/react-lights-out/static/js/main.63a84a24.chunk.js"
+  },
+  {
+    "revision": "207e5d056e3105dcde38",
+    "url": "/react-lights-out/static/js/1.207e5d05.chunk.js"
+  },
+  {
+    "revision": "1764a79bcd54ed66a092",
+    "url": "/react-lights-out/static/js/runtime~main.1764a79b.js"
+  },
+  {
+    "revision": "9f7b28fea38ee08ecd2e4bb8e489a11b",
+    "url": "/react-lights-out/index.html"
+  }
+];
